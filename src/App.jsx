ddd75129b7c0335b57fcf62022ad6a1e8dee79c8
Franco_Osorio <franco95.osorio@gmail.com>
2025-06-09 19:29:12 -0300

    Se agrega vistas 'DashboardEncuesta' y 'EncuestaCreada'.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import DashboardEncuesta from './pages/DashboardEncuesta';
+import EncuestaCreada from './pages/EncuestaCreada';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/crear" element={<DashboardEncuesta />} />
+        <Route path="/encuesta-creada" element={<EncuestaCreada />} /> 
       </Routes>
     </BrowserRouter>
   );
